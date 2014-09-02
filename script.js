d54be71552798b55e8e7c6a58265ca2c57458e93
Daniel Peiró <danipeiro@gmail.com>
2014-09-02 14:52:20 +0200

    touch alternative added. organize code

--- script.js
+++ script.js
@@ -5,59 +5,61 @@ var joystickctx = joystick.getContext('2d');
 var vector = document.getElementById('vector');
 var vectorctx = vector.getContext('2d');
 var position = document.getElementById('position');
+var point = {
+	x: 0,
+	y: 0
+};
+
+var radius = 100;
+var leftClick=0;
 
 
-joystick.addEventListener('mousedown', mouseMove, false);
-joystick.addEventListener('mousemove', mouseMove, false);
+resetAll();
+
+
+joystick.addEventListener('touchstart', touch, false);
+joystick.addEventListener('touchend', resetAll, false);
+joystick.addEventListener('touchmove', touch, false);
+
+joystick.addEventListener('mousedown', function (evt) {
+	leftClick=1;
+	mouseMove(evt);
+}, false);
 joystick.addEventListener('mouseup', function () {
-	resetJoystick();
-	resetVector();
-	circle( joystickctx, 250, 250, 100, true);
+	leftClick=0;
+	resetAll();
 }, false);
 
+joystick.addEventListener('mousemove', mouseMove, false);
 
-resetJoystick();
-resetVector();
-circle( joystickctx, 250, 250, 100, true);
+
+function touch (evt) {
+	evt.preventDefault();
+	leftClick=1;
+	mouseMove(evt);
+}
+
 
 function mouseMove (evt) {
 
-	if (evt.which==1) { //check if left mouse button down.
-		var x = evt.clientX - 12;
-		var y = evt.clientY - 12;		
-		if (!checkCircle(x - 250, 250 - y, 200)) {
-			x = x - 250;
-			y = 250 - y;
-			var a = y/x;
-			if (x>0) {
-				x = 200/Math.sqrt(1+a*a) + 250;				
-			} else  {
-				x = -200/Math.sqrt(1+a*a) + 250;
-				
-			}
-			if (y>0) {
-				y = 250  - 200/Math.sqrt(1+1/(a*a));
-			} else {
-				y = 250  + 200/Math.sqrt(1+1/(a*a));
-			}
-
-			x = Math.floor(x);
-			y = Math.floor(y);
-		};
-
+	if (leftClick==1) { //check if left mouse button down.
 		resetJoystick();
-		resetVector();		
-		
-		var radius = 100;
-		var arrow = new Image();
-		arrow.src = "arrow.png";
+		resetVector();
 
-		circle(joystickctx, x, y,radius, true);
+		if (evt.type == 'touchstart' || evt.type == 'touchmove') {
+			point.x = evt.touches[0].pageX;
+			point.y = evt.touches[0].pageY;
+		} else{
+			point.x = evt.pageX - 12;
+			point.y = evt.pageY - 12;
+		};
 		
-		drawLineFromCenter(vectorctx,x,y);
-			
+		forceIntoCircle(point,200);
+
+		circle(joystickctx, point.x, point.y,radius, true);
+		drawLineFromCenter(vectorctx, point.x, point.y);
+		position.innerHTML="x:"+ (point.x -250) +" y:"+ (250 - point.y); //update position label.
 		
-		position.innerHTML="x:"+ (x -250) +" y:"+ (250 - y); //update position label.
 	};
 	
 }
@@ -67,7 +69,8 @@ function resetJoystick () {
 	joystickctx.fillRect(0,0,500,500);
 	joystickctx.fillStyle = "black";
 	circle( joystickctx, 250, 250, 200);
-	position.innerHTML="x:0 y:0";
+	x=250;
+	y=250;	
 }
 
 
@@ -78,6 +81,12 @@ function resetVector () {
 	vectorctx.fillStyle = "black";
 	vectorctx.fillRect(248,248,4,4);
 	circle(vectorctx, 250, 250, 200);
+}
+
+function resetAll () {
+	resetJoystick();
+	resetVector();
+	circle( joystickctx, 250, 250, 100, true);
 }
 
 function checkCircle (x,y,radius) {
@@ -99,4 +108,26 @@ function drawLineFromCenter (ctx, x, y) {
 	ctx.moveTo(250,250);
 	ctx.lineTo( x, y);
 	ctx.stroke();	
+}
+
+function forceIntoCircle (point,radius) {
+	if (!checkCircle(point.x - 250, 250 - point.y, radius)) {
+		var x = point.x - 250;
+		var y = 250 - point.y;
+		var a = y/x;
+		if (x>0) {
+			x = radius/Math.sqrt(1+a*a) + 250;				
+		} else  {
+			x = -radius/Math.sqrt(1+a*a) + 250;
+			
+		}
+		if (y>0) {
+			y = 250  - radius/Math.sqrt(1+1/(a*a));
+		} else {
+			y = 250  + radius/Math.sqrt(1+1/(a*a));
+		}
+
+		point.x = Math.floor(x);
+	    point.y = Math.floor(y);		
+	};
 }
