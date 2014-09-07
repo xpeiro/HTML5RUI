0bd431b1bff4ecbd52ef64b9baf5de62758db920
Daniel Peiró <danipeiro@gmail.com>
2014-09-08 00:21:29 +0200

    Format Code

--- joystick.js
+++ joystick.js
@@ -5,125 +5,121 @@ var vector = document.getElementById('vector');
 var vectorctx = vector.getContext('2d');
 var position = document.getElementById('position');
 var point = {
-	x: 0,
-	y: 0
+	x : 0,
+	y : 0
 };
 
-var maxRadius = joystick.width/2*0.8;
-var radius = joystick.width*0.3;
-var leftClick=0;
-
+var maxRadius = joystick.width / 2 * 0.8;
+var radius = joystick.width * 0.3;
+var leftClick = 0;
 
 resetAll();
 
 joystick.addEventListener('touchstart', touch, false);
 joystick.addEventListener('touchend', resetAll, false);
 joystick.addEventListener('touchmove', touch, false);
-joystick.addEventListener('mousedown', function (evt) {
-	leftClick=1;
-	mouseMove(evt);
-}, false);
-joystick.addEventListener('mouseup', function () {
-	leftClick=0;
+joystick.addEventListener('mousedown', touch, false);
+joystick.addEventListener('mouseup', function() {
+	leftClick = 0;
 	resetAll();
 }, false);
 joystick.addEventListener('mousemove', mouseMove, false);
 
-function touch (evt) {
+function touch(evt) {
 	evt.preventDefault();
-	leftClick=1;
+	leftClick = 1;
 	mouseMove(evt);
 }
 
-function mouseMove (evt) {
+function mouseMove(evt) {
 
-	if (leftClick==1) { //check if left mouse button down.
+	if (leftClick == 1) {//check if left mouse button down.
 		resetJoystick();
 		resetVector();
 
 		if (evt.type == 'touchstart' || evt.type == 'touchmove') {
 			point.x = evt.touches[0].pageX;
 			point.y = evt.touches[0].pageY;
-		} else{
+		} else {
 			point.x = evt.pageX - 12;
 			point.y = evt.pageY - 12;
 		};
-		
-		forceIntoCircle(point,maxRadius);
 
-		
+		forceIntoCircle(point, maxRadius);
+
 		drawLineFromCenter(vectorctx, point.x, point.y);
 		drawLineFromCenter(joystickctx, point.x, point.y);
-		circle(joystickctx, point.x, point.y,radius, true, "grey");
-		position.innerHTML="x:"+ (point.x -joystick.width) +" y:"+ (joystick.height - point.y); //update position label.
-		
+		circle(joystickctx, point.x, point.y, radius, "grey");
+		position.innerHTML = "x:" + (point.x - joystick.width / 2) + " y:" + (joystick.height / 2 - point.y);
+		//update position label.
+
 	};
-	
+
 }
 
-function resetJoystick () {
+function resetJoystick() {
 	joystickctx.fillStyle = "#081628";
-	joystickctx.fillRect(0,0,joystick.width,joystick.height);
+	joystickctx.fillRect(0, 0, joystick.width, joystick.height);
 	joystickctx.fillStyle = "black";
-	circle( joystickctx, joystick.width/2, joystick.height/2, maxRadius, true, "#8e98a4");
-	position.innerHTML="x:0"+" y:0"; //update position label.	
+	circle(joystickctx, joystick.width / 2, joystick.height / 2, maxRadius, "#8e98a4");
+	position.innerHTML = "x:0" + " y:0";
+	//update position label.
 }
 
-function resetVector () {
+function resetVector() {
 	vectorctx.fillStyle = "white";
-	vectorctx.fillRect(0,0,vector.width,vector.height);
+	vectorctx.fillRect(0, 0, vector.width, vector.height);
 	vectorctx.fillStyle = "black";
-	vectorctx.fillRect(vector.width/2-2,vector.height/2-2,4,4);
-	circle(vectorctx, vector.width/2, vector.height/2, maxRadius);
+	vectorctx.fillRect(vector.width / 2 - 2, vector.height / 2 - 2, 4, 4);
+	circle(vectorctx, vector.width / 2, vector.height / 2, maxRadius);
 }
 
-function resetAll () {
+function resetAll() {
 	resetJoystick();
 	resetVector();
-	circle( joystickctx, joystick.width/2, joystick.height/2, radius, true, "grey");
+	circle(joystickctx, joystick.width / 2, joystick.height / 2, radius, "grey");
 }
 
-function checkCircle (x,y,radius) {
-	return ( (x*x + y*y) <= (radius*radius));
+function checkCircle(x, y, radius) {
+	return ((x * x + y * y) <= (radius * radius));
 }
 
-function circle (ctx, x, y, radius, fill, fillColor) {
+function circle(ctx, x, y, radius, fillColor) {
 	ctx.beginPath();
-	ctx.arc( x, y, radius, 0, 2 * Math.PI, false);
-	if (fill) { 
+	ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
+	if (!!fillColor) {
 		ctx.fillStyle = fillColor;
 		ctx.fill();
 	};
 	ctx.stroke();
 }
 
-function drawLineFromCenter (ctx, x, y) {
+function drawLineFromCenter(ctx, x, y) {
 	ctx.beginPath();
-	ctx.moveTo(vector.width/2,vector.height/2);
-	ctx.lineTo( x, y);
-	ctx.stroke();	
+	ctx.moveTo(vector.width / 2, vector.height / 2);
+	ctx.lineTo(x, y);
+	ctx.stroke();
 }
 
-function forceIntoCircle (point,radius) {
-	if (!checkCircle(point.x - joystick.width/2, joystick.height/2 - point.y, radius)) {
-		point.x = point.x - joystick.width/2;
-		point.y = joystick.height/2 - point.y;
-		var a = point.y/point.x;
-		if (point.x>0) {
-			point.x = radius/Math.sqrt(1+a*a) + joystick.width/2;				
-		} else  {
-			point.x = -radius/Math.sqrt(1+a*a) + joystick.width/2;
-			
-		}
-		if (point.y>0) {
-			point.y = joystick.height/2  - radius/Math.sqrt(1+1/(a*a));
+function forceIntoCircle(point, radius) {
+	if (!checkCircle(point.x - joystick.width / 2, joystick.height / 2 - point.y, radius)) {
+		point.x = point.x - joystick.width / 2;
+		point.y = joystick.height / 2 - point.y;
+		var a = point.y / point.x;
+		if (point.x > 0) {
+			point.x = radius / Math.sqrt(1 + a * a) + joystick.width / 2;
 		} else {
-			point.y = joystick.height/2  + radius/Math.sqrt(1+1/(a*a));
+			point.x = -radius / Math.sqrt(1 + a * a) + joystick.width / 2;
+
+		}
+		if (point.y > 0) {
+			point.y = joystick.height / 2 - radius / Math.sqrt(1 + 1 / (a * a));
+		} else {
+			point.y = joystick.height / 2 + radius / Math.sqrt(1 + 1 / (a * a));
 		}
 
-		
 	};
 
 	point.x = Math.floor(point.x);
-    point.y = Math.floor(point.y);	
+	point.y = Math.floor(point.y);
 }
