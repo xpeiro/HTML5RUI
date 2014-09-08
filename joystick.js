3766b86e9702bb0d775574ffdb6672c1d6653740
Daniel Peiró <danipeiro@gmail.com>
2014-09-08 04:04:37 +0200

    Lock Joystick (canvas implementation)

--- joystick.js
+++ joystick.js
@@ -4,9 +4,10 @@ var joystickctx = joystick.getContext('2d');
 var vector = document.getElementById('vector');
 var vectorctx = vector.getContext('2d');
 var position = document.getElementById('position');
+var isLocked = false;
 var point = {
 	x : 0,
-	y : 0
+	y : 0,
 };
 
 var maxRadius = joystick.width / 2 * 0.8;
@@ -15,15 +16,27 @@ var leftClick = 0;
 
 resetAll();
 
-joystick.addEventListener('touchstart', touch, false);
-joystick.addEventListener('touchend', resetAll, false);
+joystick.addEventListener('touchstart', mouseDown, false);
 joystick.addEventListener('touchmove', touch, false);
-joystick.addEventListener('mousedown', touch, false);
-joystick.addEventListener('mouseup', function() {
-	leftClick = 0;
-	resetAll();
-}, false);
+joystick.addEventListener('touchend', mouseUp, false);
+joystick.addEventListener('mousedown', mouseDown, false);
 joystick.addEventListener('mousemove', mouseMove, false);
+joystick.addEventListener('mouseup', mouseUp, false);
+
+function mouseDown(evt) {
+	if (isInsideSquare(evt.pageX - 12, evt.pageY- 12 - joystick.height*0.97, joystick.width*0.05)) {
+		isLocked = !isLocked;
+		resetAll();
+	} else {
+		leftClick=1;
+		mouseMove(evt);
+	}
+}
+
+function mouseUp() {
+	leftClick=0;
+	if (!isLocked) resetAll(); 	
+}
 
 function touch(evt) {
 	evt.preventDefault();
@@ -33,7 +46,7 @@ function touch(evt) {
 
 function mouseMove(evt) {
 
-	if (leftClick == 1) {//check if left mouse button down.
+	if (leftClick == 1) {//check if left mouse button down or touch
 		resetJoystick();
 		resetVector();
 
@@ -45,13 +58,16 @@ function mouseMove(evt) {
 			point.y = evt.pageY - 12;
 		};
 
-		forceIntoCircle(point, maxRadius);
-
+		point = centerCoord(point,joystick);
+		point = forceIntoCircle(point.x, point.y, maxRadius);
+		point = canvasCoord(point,joystick);
+		
 		drawLineFromCenter(vectorctx, point.x, point.y);
 		drawLineFromCenter(joystickctx, point.x, point.y);
-		circle(joystickctx, point.x, point.y, radius, "grey");
-		position.innerHTML = "x:" + (point.x - joystick.width / 2) + " y:" + (joystick.height / 2 - point.y);
-		//update position label.
+		drawCircle(joystickctx, point.x, point.y, radius, "grey");
+		point = centerCoord(point,joystick);
+		//vectorctx.font=joystick.height*0.05+"px Arial";
+		vectorctx.fillText("x:" + point.x.toFixed(2) + " y:" + point.y.toFixed(2),vector.width*0.02,vector.height*0.1);//update position label.
 
 	};
 
@@ -61,9 +77,8 @@ function resetJoystick() {
 	joystickctx.fillStyle = "#081628";
 	joystickctx.fillRect(0, 0, joystick.width, joystick.height);
 	joystickctx.fillStyle = "black";
-	circle(joystickctx, joystick.width / 2, joystick.height / 2, maxRadius, "#8e98a4");
-	position.innerHTML = "x:0" + " y:0";
-	//update position label.
+	drawCircle(joystickctx, joystick.width / 2, joystick.height / 2, maxRadius, "#8e98a4");
+	drawLock();
 }
 
 function resetVector() {
@@ -71,20 +86,26 @@ function resetVector() {
 	vectorctx.fillRect(0, 0, vector.width, vector.height);
 	vectorctx.fillStyle = "black";
 	vectorctx.fillRect(vector.width / 2 - 2, vector.height / 2 - 2, 4, 4);
-	circle(vectorctx, vector.width / 2, vector.height / 2, maxRadius);
+	drawCircle(vectorctx, vector.width / 2, vector.height / 2, maxRadius);
 }
 
 function resetAll() {
+	leftClick = 0;
 	resetJoystick();
 	resetVector();
-	circle(joystickctx, joystick.width / 2, joystick.height / 2, radius, "grey");
+	drawCircle(joystickctx, joystick.width / 2, joystick.height / 2, radius, "grey");
+	
 }
 
-function checkCircle(x, y, radius) {
+function isInsideCircle(x, y, radius) {
 	return ((x * x + y * y) <= (radius * radius));
 }
 
-function circle(ctx, x, y, radius, fillColor) {
+function isInsideSquare (x,y,l) {
+  return (Math.abs(x) <= l && Math.abs(y) <= l);	
+}
+
+function drawCircle(ctx, x, y, radius, fillColor) {
 	ctx.beginPath();
 	ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
 	if (!!fillColor) {
@@ -101,25 +122,56 @@ function drawLineFromCenter(ctx, x, y) {
 	ctx.stroke();
 }
 
-function forceIntoCircle(point, radius) {
-	if (!checkCircle(point.x - joystick.width / 2, joystick.height / 2 - point.y, radius)) {
-		point.x = point.x - joystick.width / 2;
-		point.y = joystick.height / 2 - point.y;
-		var a = point.y / point.x;
-		if (point.x > 0) {
-			point.x = radius / Math.sqrt(1 + a * a) + joystick.width / 2;
+function forceIntoCircle(x, y, radius) {
+	if (!isInsideCircle(x, y, radius)) {
+		var a = y / x;
+		if (x > 0) {
+			x = radius / Math.sqrt(1 + a * a);
 		} else {
-			point.x = -radius / Math.sqrt(1 + a * a) + joystick.width / 2;
+			x = -radius / Math.sqrt(1 + a * a);
 
 		}
-		if (point.y > 0) {
-			point.y = joystick.height / 2 - radius / Math.sqrt(1 + 1 / (a * a));
+		if (y > 0) {
+			y = radius / Math.sqrt(1 + 1 / (a * a));
 		} else {
-			point.y = joystick.height / 2 + radius / Math.sqrt(1 + 1 / (a * a));
+			y = -radius / Math.sqrt(1 + 1 / (a * a));
 		}
 
 	};
 
-	point.x = Math.floor(point.x);
-	point.y = Math.floor(point.y);
+	return {
+		x : x,
+		y : y
+	};
 }
+
+function centerCoord(point, canvas) {
+	return {
+		x: point.x - canvas.width / 2,
+		y: canvas.height / 2 - point.y,
+	};
+}
+
+function canvasCoord(point, canvas) {
+	return {
+		x: point.x + canvas.width / 2,
+		y: canvas.height / 2 - point.y,
+	};
+	
+}
+
+function drawLock () {
+	if (isLocked) {
+		joystickctx.fillStyle = "red";
+		joystickctx.fillRect(0,joystick.height*0.93, joystick.width*0.05, joystick.width*0.05);
+		joystickctx.fillStyle = "grey";
+		joystickctx.fillText("Lock On", joystick.width*0.06,joystick.height*0.97);
+	  	
+	} else {
+		joystickctx.fillStyle = "green";
+		joystickctx.fillRect(0,joystick.height*0.93, joystick.width*0.05, joystick.width*0.05);
+		joystickctx.fillStyle = "grey";
+		joystickctx.fillText("Lock Off", joystick.width*0.06,joystick.height*0.97);
+	}
+}
+
